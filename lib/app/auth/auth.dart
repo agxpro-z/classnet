5dbf9adc9243c343b55458466f28eb7327e642f0
AnkitGourav <Ankitgourav035@gmail.com>
2024-03-11 23:56:33 +0530

    HomePage: Add option to logout.
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- lib/app/auth/auth.dart
+++ lib/app/auth/auth.dart
@@ -1,7 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../../res/values/strings.dart';
+
 class Auth {
+  static String getUserEmail() {
+    return FirebaseAuth.instance.currentUser?.email ?? Strings.invalidEmail;
+  }
+
+  static String getUserName() {
+    return FirebaseAuth.instance.currentUser?.displayName ?? Strings.invalidUser;
+  }
+
   static Future<void> signIn(String email, String password) async {
     await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  static Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
   }
 }
--- lib/res/values/strings.dart
+++ lib/res/values/strings.dart
@@ -8,4 +8,14 @@ class Strings {
   static const String forgotPassword = "Forgot password?";
   static const String createAccount = "Create account";
   static const String login = "Log-in";
+
+  // Sign-out
+  static const String signOut = "Log out";
+
+  // Home
+  static const String home = "Home";
+
+  // User
+  static const invalidUser = "Invalid user";
+  static const invalidEmail = "Invalid e-mail";
 }
