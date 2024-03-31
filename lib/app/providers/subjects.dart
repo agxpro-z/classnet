1c810839693b9f06ce7ab56cc16f2d63a711e1b6
AnkitGourav <Ankitgourav035@gmail.com>
2024-03-31 18:45:42 +0530

    Show subjects taught by teacher when email belongs to teacher.
    If email doesn't have 'student' in it then the email belongs to faculty.
    If faculty email is detected then show subjects taught by him/her in home page.
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- lib/app/providers/subjects.dart
+++ lib/app/providers/subjects.dart
@@ -43,4 +43,40 @@ class SubjectsProvider {
     final data =  await getSubjects(collection, sem);
     return data[subId];
   }
+
+  Future<List<Map<String, dynamic>>> getTeacherSubjects(String collection, String id, String ay) async {
+    final data = await FirebaseFirestore.instance
+        .collection(collection)
+        .doc(id)
+        .collection(ay)
+        .get();
+
+    List<Map<String, dynamic>> subjects = [];
+
+    for (var snapshot in data.docs) {
+      final snapshotData = snapshot.data();
+      Map<String, dynamic> subjectData = {};
+
+      await (snapshotData['course'] as DocumentReference<Map<String, dynamic>>).get().then((value) {
+        subjectData['department'] = value.data()?['department'];
+        subjectData['departmentName'] = value.data()?['departmentName'];
+        subjectData['course'] = value.data()?['course'];
+        subjectData['courseName'] = value.data()?['courseName'];
+      });
+
+      await (snapshotData['semRef'] as DocumentReference<Map<String, dynamic>>).get().then((value) async {
+        subjectData['title'] = value.data()?[snapshotData['subId']]['title'];
+
+        String? collection = value.data()?[snapshotData['subId']]?['collection'];
+        // print(collection);
+        if (collection != null) {
+          subjectData['doc'] = value.reference.collection(collection);
+        }
+      });
+
+      subjects.add(subjectData);
+    }
+
+    return subjects;
+  }
 }
