c4d02e5e2cffe06b3e9d86ef75b8c2645a78e8be
AnkitGourav <Ankitgourav035@gmail.com>
2024-04-15 17:40:28 +0530

    Widget: SubjectTileList: Wrap in Card widget.
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- lib/app/ui/widgets/subjects/subject_list_tile.dart
+++ lib/app/ui/widgets/subjects/subject_list_tile.dart
@@ -22,53 +22,56 @@ class SubjectListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: <Widget>[
-          Container(
-            height: 48,
-            width: 48,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              color: theme.colorScheme.primaryContainer,
-            ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width - 104,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: theme.textTheme.titleSmall?.fontSize,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  if (!forStudent)
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 4.0),
-                      child: Text(
-                        "$course ($department)",
-                        style: TextStyle(
-                          fontSize: theme.textTheme.bodySmall?.fontSize,
-                        ),
-                      ),
-                    ),
-                  Text(
-                    "${Strings.assignments}: ${assignments.toString()}",
-                    style: TextStyle(
-                      fontSize: theme.textTheme.bodySmall?.fontSize,
-                    ),
-                  ),
-                ],
+    return Card(
+      elevation: 0.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+              height: 48,
+              width: 48,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: theme.colorScheme.primaryContainer,
               ),
             ),
-          )
-        ],
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 104,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: theme.textTheme.titleSmall?.fontSize,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    if (!forStudent)
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4.0),
+                        child: Text(
+                          "$course ($department)",
+                          style: TextStyle(
+                            fontSize: theme.textTheme.bodySmall?.fontSize,
+                          ),
+                        ),
+                      ),
+                    Text(
+                      "${Strings.assignments}: ${assignments.toString()}",
+                      style: TextStyle(
+                        fontSize: theme.textTheme.bodySmall?.fontSize,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
