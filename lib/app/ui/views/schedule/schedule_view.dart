ce13faf2bd2e4d9cd474dfc004df7e62390880a9
AnkitGourav <Ankitgourav035@gmail.com>
2024-04-21 03:13:58 +0530

    ScheduleView: Pull down to refresh.
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- lib/app/ui/views/schedule/schedule_view.dart
+++ lib/app/ui/views/schedule/schedule_view.dart
@@ -32,117 +32,120 @@ class _ScheduleViewState extends State<ScheduleView> {
       onViewModelReady: (viewModel) => viewModel.initialize(),
       viewModelBuilder: () => locator<ScheduleViewModel>(),
       builder: (BuildContext context, ScheduleViewModel viewModel, Widget? child) => Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            CustomSliverAppBar(
-              isMainView: true,
-              title: Text(
-                t.scheduleView.schedule,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onSurface,
+        body: RefreshIndicator(
+          onRefresh: () => viewModel.fetchScheduleForDay(viewModel.taskDay),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              CustomSliverAppBar(
+                isMainView: true,
+                title: Text(
+                  t.scheduleView.schedule,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
                 ),
               ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate.fixed(
-                <Widget>[
-                  TableCalendar(
-                    focusedDay: _focusedDay,
-                    firstDay: DateTime.utc(1970),
-                    lastDay: DateTime.utc(2030),
-                    rowHeight: 40.0,
-                    calendarFormat: _calendarFormat,
-                    onFormatChanged: (format) => setState(() {
-                      _calendarFormat = format;
-                    }),
-                    calendarStyle: CalendarStyle(
-                      cellMargin: const EdgeInsets.all(2.0),
-                      weekendTextStyle: TextStyle(
-                        color: theme.colorScheme.error,
+              SliverList(
+                delegate: SliverChildListDelegate.fixed(
+                  <Widget>[
+                    TableCalendar(
+                      focusedDay: _focusedDay,
+                      firstDay: DateTime.utc(1970),
+                      lastDay: DateTime.utc(2030),
+                      rowHeight: 40.0,
+                      calendarFormat: _calendarFormat,
+                      onFormatChanged: (format) => setState(() {
+                        _calendarFormat = format;
+                      }),
+                      calendarStyle: CalendarStyle(
+                        cellMargin: const EdgeInsets.all(2.0),
+                        weekendTextStyle: TextStyle(
+                          color: theme.colorScheme.error,
+                        ),
+                        todayDecoration: BoxDecoration(
+                          color: theme.colorScheme.primaryContainer,
+                          shape: BoxShape.circle,
+                        ),
+                        todayTextStyle: TextStyle(
+                          color: theme.colorScheme.onPrimaryContainer,
+                        ),
+                        selectedDecoration: BoxDecoration(
+                          color: theme.colorScheme.primary,
+                          shape: BoxShape.circle,
+                        ),
+                        selectedTextStyle: TextStyle(
+                          color: theme.colorScheme.onPrimary,
+                        ),
                       ),
-                      todayDecoration: BoxDecoration(
-                        color: theme.colorScheme.primaryContainer,
-                        shape: BoxShape.circle,
+                      daysOfWeekHeight: 28.0,
+                      daysOfWeekStyle: DaysOfWeekStyle(
+                        weekendStyle: TextStyle(
+                          color: theme.colorScheme.error,
+                        ),
                       ),
-                      todayTextStyle: TextStyle(
-                        color: theme.colorScheme.onPrimaryContainer,
-                      ),
-                      selectedDecoration: BoxDecoration(
-                        color: theme.colorScheme.primary,
-                        shape: BoxShape.circle,
-                      ),
-                      selectedTextStyle: TextStyle(
-                        color: theme.colorScheme.onPrimary,
-                      ),
-                    ),
-                    daysOfWeekHeight: 28.0,
-                    daysOfWeekStyle: DaysOfWeekStyle(
-                      weekendStyle: TextStyle(
-                        color: theme.colorScheme.error,
-                      ),
-                    ),
-                    selectedDayPredicate: (day) {
-                      return isSameDay(_selectedDay, day);
-                    },
-                    onDaySelected: (selectedDay, focusedDay) {
-                      setState(() async {
-                        _selectedDay = selectedDay;
-                        _focusedDay = focusedDay; // update `_focusedDay` here as well
+                      selectedDayPredicate: (day) {
+                        return isSameDay(_selectedDay, day);
+                      },
+                      onDaySelected: (selectedDay, focusedDay) {
+                        setState(() async {
+                          _selectedDay = selectedDay;
+                          _focusedDay = focusedDay; // update `_focusedDay` here as well
 
-                        await viewModel.fetchScheduleForDay(DateFormat('EEE').format(focusedDay));
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  if (viewModel.isBusy)
-                    const Center(child: CircularProgressIndicator())
-                  else
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0, right: 8.0),
-                      child: FixedTimeline.tileBuilder(
-                        theme: TimelineThemeData(
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                        builder: TimelineTileBuilder.connectedFromStyle(
-                          contentsAlign: ContentsAlign.basic,
-                          connectionDirection: ConnectionDirection.after,
-                          nodePositionBuilder: (context, index) => 0.0,
-                          firstConnectorStyle: ConnectorStyle.transparent,
-                          lastConnectorStyle: ConnectorStyle.transparent,
-                          contentsBuilder: (context, index) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TaskCard(
-                              task: viewModel.taskList[index],
-                            ),
+                          await viewModel.fetchScheduleForDay(DateFormat('EEE').format(focusedDay));
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    if (viewModel.isBusy)
+                      const Center(child: CircularProgressIndicator())
+                    else
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, right: 8.0),
+                        child: FixedTimeline.tileBuilder(
+                          theme: TimelineThemeData(
+                            color: Theme.of(context).colorScheme.primary,
                           ),
-                          connectorStyleBuilder: (context, index) {
-                            if (DateFormat('EEE').format(DateTime.now()) != viewModel.taskDay) {
-                              return ConnectorStyle.solidLine;
-                            }
-                            return viewModel.taskList[index].time.toString().compareTo(TimeOfDay.now().toString()) < 0 &&
-                                    viewModel.taskList[index].time.toString().compareTo(
-                                            (TimeOfDay(hour: TimeOfDay.now().hour - 1, minute: TimeOfDay.now().minute))
-                                                .toString()) >
-                                        0
-                                ? ConnectorStyle.dashedLine
-                                : ConnectorStyle.solidLine;
-                          },
-                          indicatorStyleBuilder: (context, index) {
-                            if (DateFormat('EEE').format(DateTime.now()) != viewModel.taskDay) {
-                              return IndicatorStyle.outlined;
-                            }
-                            return viewModel.taskList[index].time.toString().compareTo(TimeOfDay.now().toString()) < 0
-                                ? IndicatorStyle.dot
-                                : IndicatorStyle.outlined;
-                          },
-                          itemCount: viewModel.taskList.length,
+                          builder: TimelineTileBuilder.connectedFromStyle(
+                            contentsAlign: ContentsAlign.basic,
+                            connectionDirection: ConnectionDirection.after,
+                            nodePositionBuilder: (context, index) => 0.0,
+                            firstConnectorStyle: ConnectorStyle.transparent,
+                            lastConnectorStyle: ConnectorStyle.transparent,
+                            contentsBuilder: (context, index) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TaskCard(
+                                task: viewModel.taskList[index],
+                              ),
+                            ),
+                            connectorStyleBuilder: (context, index) {
+                              if (DateFormat('EEE').format(DateTime.now()) != viewModel.taskDay) {
+                                return ConnectorStyle.solidLine;
+                              }
+                              return viewModel.taskList[index].time.toString().compareTo(TimeOfDay.now().toString()) < 0 &&
+                                      viewModel.taskList[index].time.toString().compareTo(
+                                              (TimeOfDay(hour: TimeOfDay.now().hour - 1, minute: TimeOfDay.now().minute))
+                                                  .toString()) >
+                                          0
+                                  ? ConnectorStyle.dashedLine
+                                  : ConnectorStyle.solidLine;
+                            },
+                            indicatorStyleBuilder: (context, index) {
+                              if (DateFormat('EEE').format(DateTime.now()) != viewModel.taskDay) {
+                                return IndicatorStyle.outlined;
+                              }
+                              return viewModel.taskList[index].time.toString().compareTo(TimeOfDay.now().toString()) < 0
+                                  ? IndicatorStyle.dot
+                                  : IndicatorStyle.outlined;
+                            },
+                            itemCount: viewModel.taskList.length,
+                          ),
                         ),
-                      ),
-                    )
-                ],
+                      )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
